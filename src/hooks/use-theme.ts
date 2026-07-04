10c102f047a2df67fb45ca38223e2e377280395c
Ayush Kumar Sharma <ayushsharma192004@gmail.com>
2026-07-04 20:20:17 +0530

    updated theme imports

--- src/hooks/use-theme.ts
+++ src/hooks/use-theme.ts
@@ -1,7 +1,4 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
+
 import { useMemo } from 'react';
 import { Colors, type ThemeColors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
